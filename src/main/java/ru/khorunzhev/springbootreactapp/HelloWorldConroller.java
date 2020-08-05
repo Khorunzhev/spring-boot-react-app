c545518e568e02cf9a8db85eb642aaa44e91d826
horunzhev <horunzhev@tecforce.ru>
2020-08-05 17:14:50 +0400

    in progress

---
+++ src/main/java/ru/khorunzhev/springbootreactapp/HelloWorldConroller.java
@@ -0,0 +1,24 @@
+package ru.khorunzhev.springbootreactapp;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class HelloWorldConroller {
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("HelloWorld");
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("HelloWorld, %s", name));
+    }
+
+}
